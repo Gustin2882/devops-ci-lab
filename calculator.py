11547813b00ae2fbbad6385631bc1cc7d334d996
Dustin Lojewski <dustin.lojewski@gmail.com>
2026-02-03 18:54:25 +0100

    Fix linting issues

--- calculator.py
+++ calculator.py
@@ -1,6 +1,8 @@
 # calculator.py (BROKEN VERSION)
 def add(x, y):
-    return x - y # <--- Oops! I used minus instead of plus
+    return x - y  # <--- Oops! I used minus instead of plus
+
+
 def subtract(x, y):
-    print(f"\n--- DEBUG: Calculating {x} - {y} ---")
+    print("\n--- DEBUG: Calculating {x} - {y} ---")
     return x - y
