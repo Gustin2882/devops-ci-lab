debdac13f6c0fb9289d0375b50f3b75b5725db0a
Dustin Lojewski <dustin.lojewski@gmail.com>
2026-02-03 18:16:25 +0100

    Optimized the add function

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
-# calculator.py
+# calculator.py (BROKEN VERSION)
 def add(x, y):
-    print(f"\n--- DEBUG: Calculating {x} + {y} ---")
-    return x + y
+    return x - y # <--- Oops! I used minus instead of plus
 def subtract(x, y):
     print(f"\n--- DEBUG: Calculating {x} - {y} ---")
     return x - y
